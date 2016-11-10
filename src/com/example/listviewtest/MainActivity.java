11971884f6ec6a4e5b08f46c13b9b70a66d51ff3
MTmotong <dzymt6627@163.com>
2016-11-10 22:39:26 +0800

    no conflict

--- src/com/example/listviewtest/MainActivity.java
+++ src/com/example/listviewtest/MainActivity.java
@@ -1,45 +1,49 @@
 package com.example.listviewtest;
 
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 	
 	
-	private List<Fruit> fruitList = new ArrayList<Fruit>();
-	/*private String[] data = { "Apple", "Banana", "Orange", "Watermelon", "Pear", "plum", "lemon", "lychee", 
-			"Grape", "Pineapple", "Strawberry", "Cherry", "Mango", "date", "mulberry" };*/
+	private String[] data = { "Apple", "Banana", "Orange", "Watermelon", "Pear", "plum", "lemon", "lychee", 
+			"Grape", "Pineapple", "Strawberry", "Cherry", "Mango", "date", "mulberry" };
+	List<String> datalist = Arrays.asList(data);  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initFruits(); //初始化水果数据
-        FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
-       /* ArrayAdapter<String> adapter = new ArrayAdapter<String> (
-        		MainActivity.this, android.R.layout.simple_list_item_1, data);*/
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (
+        		MainActivity.this, android.R.layout.simple_list_item_1, data);
         ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);	
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        	@Override
+        	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        		String name = datalist.get(position);
+        		Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+        	}
+        });
+      
+
     }
 
-    private void initFruits() {
-    	Fruit apple = new Fruit("Apple", R.drawable.apple);
-    	fruitList.add(apple);
-    	Fruit banana = new Fruit("Banana", R.drawable.banana);
-    	fruitList.add(banana);
-    	Fruit orange = new Fruit("Orange", R.drawable.orange);
-    	fruitList.add(orange);
-    	Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon);
-    	fruitList.add(watermelon);
-    }
+   
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
