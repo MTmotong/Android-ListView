64d151e7e07cb88b62dd5c896dc516e8fcfe430e
MTmotong <dzymt6627@163.com>
2016-11-10 17:41:10 +0800

    add a list

--- src/com/example/listviewtest/MainActivity.java
+++ src/com/example/listviewtest/MainActivity.java
@@ -4,14 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
+	
+	
+	private String[] data = { "Apple", "Banana", "Orange", "Watermelon", "Pear", "plum", "lemon", "lychee", 
+			"Grape", "Pineapple", "Strawberry", "Cherry", "Mango", "date", "mulberry" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (
+        		MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        		
     }
 
 
